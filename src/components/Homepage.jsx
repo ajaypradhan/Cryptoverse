cd106899e24eb9b2943b461648e12a878fea4dd4
Ajay Pradhan <pradhanajay14@gmail.com>
2021-09-24 23:47:21 +0530

    till homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const Homepage = () => {
-    return (
-        <div>
-            homwpage
-        </div>
-    )
-}
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+        <Row>
+          <Col span={12}>
+            <Statistic title="Total Cryptocurrencies" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Total Exchanges" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Total Market Cap" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Total 24h Volume" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Total Markets" value="5" />
+          </Col>
+        </Row>
+      </Title>
+    </>
+  );
+};
 
-export default Homepage
+export default Homepage;
